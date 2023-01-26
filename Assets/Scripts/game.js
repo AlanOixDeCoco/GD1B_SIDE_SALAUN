a50fc005dd53dc1569d633d8b51635264e5b6552
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-01-26 13:04:39 +0100

    Update gamepad & controller handling

--- Assets/Scripts/game.js
+++ Assets/Scripts/game.js
@@ -9,16 +9,18 @@ const PALETTE_COLOR = 0x000000;
 // #endregion
 
 // #region GAME VARIABLES
-var inputX = 0, inputY = 0;
+var player;
+var inputX = 0;
+var inputJump = false;
 
 var cameraGameplay, cameraHUD;
 
-var score = 0;
-// #endregion
+var keyboardKeys;
 
-function DebugObstacle(){
-    console.log("Collides with obstacle!");
-}
+var gamepadConnected = false;
+var gamepad;
+var gamepadButtons;
+// #endregion
 
 // #region GAME SCENES
 
@@ -43,7 +45,10 @@ class GameScene extends Phaser.Scene {
         // importing spritesheets
         this.load.spritesheet('pickables','Assets/Sprites/pickables.png', { frameWidth: 32, frameHeight: 32 }); // for the pickables
         this.load.spritesheet('player','Assets/Sprites/player.png', { frameWidth: 32, frameHeight: 48 }); // for the player character
+    }
 
+    // This function is called one time after the preload scene, it is suitable for creating objects instances and generating the static environment
+    create(){
         // deactivating the scene's main camera
         this.cameras.main.setVisible(false);
         
@@ -51,11 +56,23 @@ class GameScene extends Phaser.Scene {
         cameraGameplay = this.cameras.add(0, 0, GAME_WIDTH, GAME_HEIGHT);
         cameraGameplay.setRoundPixels(true);
         this.cameraFilter = cameraGameplay.id;
-    }
-
-    // This function is called one time after the preload scene, it is suitable for creating objects instances and generating the static environment
-    create(){
         cameraGameplay.setBounds(0, 0, 1600, 1600); // set the camera border to fit in the tilemap dimensions
+
+        // setup the controller state events
+        this.input.gamepad.on('connected', () => {
+            gamepad = this.input.gamepad.pad1;
+            onGamepadConnect();
+        });
+        this.input.gamepad.on('disconnected', onGamepadDisconnect);
+
+        // setup the keys
+        keyboardKeys = this.input.keyboard.addKeys({
+            up: Phaser.Input.Keyboard.KeyCodes.UP, 
+            left: Phaser.Input.Keyboard.KeyCodes.LEFT, 
+            down: Phaser.Input.Keyboard.KeyCodes.DOWN, 
+            right: Phaser.Input.Keyboard.KeyCodes.RIGHT, 
+            jump: Phaser.Input.Keyboard.KeyCodes.SPACE
+        });
 
         // #region MAP GENERATION
         // loading the tilemap
@@ -130,14 +147,20 @@ class GameScene extends Phaser.Scene {
             pickable.anims.play(pickable.name); // --> display the right animation based on the object name
         });
 
+        const stars = this.physics.add.staticGroup();
+        pickables.forEach((pickable) => {
+            stars.add(pickable);
+        });
+
         // #endregion
 
         // #region PLAYER CREATION
-        var player = this.physics.add.sprite(160, 1400, 'player');
+        player = this.physics.add.sprite(120, 1300, 'player');
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
         this.physics.add.collider(player, [wallsLayer1, wallsLayer2, wallsLayer3, platformsLayer]); // add colision between player and ground surfaces
         this.physics.add.collider(player, obstaclesLayer, DebugObstacle); // add colision between player and obstacles
+        this.physics.add.overlap(player, pickables, () => {console.log("Take coin!")});
         // #endregion
 
         // start HUD scene on top
@@ -147,7 +170,6 @@ class GameScene extends Phaser.Scene {
         cameraGameplay.startFollow(player);
 
         // #region DEBUG
-
         const debugGraphics = this.add.graphics().setAlpha(0.75);
         obstaclesLayer.renderDebug(debugGraphics, {
             tileColor: null, // Color of non-colliding tiles
@@ -155,12 +177,17 @@ class GameScene extends Phaser.Scene {
             faceColor: new Phaser.Display.Color(255, 0, 0, 255) // Color of colliding face edges
         });
         // #endregion
+
+        // keyboard events
+        this.input.keyboard.on('keydown', onKey);
+        this.input.keyboard.on('keyup', onKey);
     }
 
     update(time){
-        // Input handling at first
-        
-        // Then calling the needed functions
+        // Input handling through events
+
+        // Then apply inputs
+        console.log(`X: ${inputX}, JUMP: ${inputJump}`);
     }
 }
 
@@ -198,6 +225,50 @@ class GameHUDScene extends Phaser.Scene {
         // Then calling the desired functions
     }
 }
+// #endregion
+
+// #region FUNCTIONS
+function DebugObstacle(){
+    console.log("Collides with obstacle!");
+}
+
+// configure the controller when it is connected
+function onGamepadConnect(){
+    console.log("Controller connected!");
+
+    // see https://phaser.io/examples/v3/view/input/gamepad/gamepad-debug to identify the buttons indexes
+    gamepadButtons = {
+        'left': gamepad.buttons[14],
+        'right': gamepad.buttons[15],
+        'jump': gamepad.buttons[0],
+        'pause': gamepad.buttons[9]
+    };
+
+    // register to the button press & release events to optimize input handling
+    gamepad.on('down', onButton);
+    gamepad.on('up', onButton);
+
+    gamepadConnected = true;
+}
+
+function onGamepadDisconnect(){
+    console.log("Controller disconnected!");
+    gamepadConnected = false;
+
+    inputX = 0; // reset the x input
+}
+
+function onButton(){
+    inputX = gamepadButtons.right.pressed - gamepadButtons.left.pressed;
+    inputJump = gamepadButtons.jump.pressed;
+}
+
+function onKey(){
+    inputX = keyboardKeys.right.isDown - keyboardKeys.left.isDown;
+    inputJump = keyboardKeys.jump.isDown;
+}
+
+//function onKeyDown()
 // #endregion
 
 // #region GAME CONFIGURATION
