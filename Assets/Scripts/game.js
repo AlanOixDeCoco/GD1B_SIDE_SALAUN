c4920e314b71a2ad2efe9adeb5297e64869d363c
AlanOixDeCoco <49500226+AlanOixDeCoco@users.noreply.github.com>
2023-01-27 20:08:23 +0100

    Update game.js

--- Assets/Scripts/game.js
+++ Assets/Scripts/game.js
@@ -1,4 +1,10 @@
 // #region CONSTANTS
+const DEBUG = false;
+
+const INPUT_ZERO_TOLERANCE = 0.1;
+
+const TILE_SIZE = 32;
+
 const GAME_WIDTH = 480;
 const GAME_HEIGHT = 320;
 
@@ -10,8 +16,14 @@ const PALETTE_COLOR = 0x000000;
 
 // #region GAME VARIABLES
 var player;
+var accelerationForce = 1024;
+var jumpForce = 320;
+var dragForce = 800;
+var maxSpeed = 128;
+
 var inputX = 0;
-var inputJump = false;
+var inputJump = 0;
+
 
 var cameraGameplay, cameraHUD;
 
@@ -67,10 +79,10 @@ class GameScene extends Phaser.Scene {
 
         // setup the keys
         keyboardKeys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.UP, 
-            left: Phaser.Input.Keyboard.KeyCodes.LEFT, 
-            down: Phaser.Input.Keyboard.KeyCodes.DOWN, 
-            right: Phaser.Input.Keyboard.KeyCodes.RIGHT, 
+            up: Phaser.Input.Keyboard.KeyCodes.Z, 
+            left: Phaser.Input.Keyboard.KeyCodes.Q, 
+            down: Phaser.Input.Keyboard.KeyCodes.S, 
+            right: Phaser.Input.Keyboard.KeyCodes.D, 
             jump: Phaser.Input.Keyboard.KeyCodes.SPACE
         });
 
@@ -119,7 +131,6 @@ class GameScene extends Phaser.Scene {
         // #endregion
 
         // #region PICKABLES CREATION
-
         // creating the pickables animations
         this.anims.create({
             key: 'coin_0',
@@ -151,7 +162,6 @@ class GameScene extends Phaser.Scene {
         pickables.forEach((pickable) => {
             stars.add(pickable);
         });
-
         // #endregion
 
         // #region PLAYER CREATION
@@ -159,23 +169,32 @@ class GameScene extends Phaser.Scene {
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
         this.physics.add.collider(player, [wallsLayer1, wallsLayer2, wallsLayer3, platformsLayer]); // add colision between player and ground surfaces
-        this.physics.add.collider(player, obstaclesLayer, DebugObstacle); // add colision between player and obstacles
+        this.physics.add.collider(player, obstaclesLayer, () => {
+            if(DEBUG) DebugObstacle();
+        }); 
+        player.setMaxVelocity(maxSpeed, 9999);
+        player.setDragX(dragForce);
+        // add colision between player and obstacles
         this.physics.add.overlap(player, pickables, () => {console.log("Take coin!")});
         // #endregion
 
         // start HUD scene on top
         game.scene.start('GameHUD');
 
-        //cameraGameplay.setZoom(.2);
-        cameraGameplay.startFollow(player);
+        // Debug
+        if(DEBUG){
+            game.scene.start('Debug');
 
-        // #region DEBUG
-        const debugGraphics = this.add.graphics().setAlpha(0.75);
-        obstaclesLayer.renderDebug(debugGraphics, {
-            tileColor: null, // Color of non-colliding tiles
-            collidingTileColor: new Phaser.Display.Color(255, 0, 0, 64), // Color of colliding tiles
-            faceColor: new Phaser.Display.Color(255, 0, 0, 255) // Color of colliding face edges
-        });
+            const debugGraphics = this.add.graphics().setAlpha(0.75);
+            obstaclesLayer.renderDebug(debugGraphics, {
+                tileColor: null, // Color of non-colliding tiles
+                collidingTileColor: new Phaser.Display.Color(255, 0, 0, 64), // Color of colliding tiles
+                faceColor: new Phaser.Display.Color(255, 0, 0, 255) // Color of colliding face edges
+            });
+        }
+        cameraGameplay.startFollow(player, true, 0.1, 0.1);
+
+        
         // #endregion
 
         // keyboard events
@@ -187,9 +206,14 @@ class GameScene extends Phaser.Scene {
 
     update(time){
         // Input handling through events
+        console.log(`${inputX} / ${inputJump}`);
 
         // Then apply inputs
-        console.log(`X: ${inputX}, JUMP: ${inputJump}`);
+        player.body.setAccelerationX(inputX * accelerationForce);
+
+        if(inputJump && player.body.blocked.down)
+            player.body.setVelocityY(inputJump * -jumpForce);
+        
     }
 }
 
@@ -217,7 +241,7 @@ class GameHUDScene extends Phaser.Scene {
 
     // This function is called one time after the preload scene, it is suitable for creating objects instances and generating the static environment
     create(){
-        this.fpsText = this.add.bitmapText(10, 10, 'CursedScript', 'FPS: ', FONT_SIZE_TITLE).setTint(0xFFFFFF);
+        
     }
 
     update(time){
@@ -225,6 +249,36 @@ class GameHUDScene extends Phaser.Scene {
         // --> The HUD scene focuses on UI controls (pause, settings buttons, ...)
 
         // Then calling the desired functions
+    }
+}
+
+// This scene contains the game HUD, it is used to display informations to the player while keeping this logic away from the gameplay
+class DebugScene extends Phaser.Scene {
+    constructor (){
+        super({key: 'Debug', active: false});
+
+        this.lastFrameTime = 0;
+        this.deltaTime = 0;
+    }
+
+    // This function is called one time at the beginning of scene start, it is suitable for assets loading
+    preload(){
+        console.log("Loading debug scene...");
+
+        // importing custom fonts
+        this.load.bitmapFont('CursedScript', 'Assets/Fonts/CursedScript.png', 'Assets/Fonts/CursedScript.fnt');
+    }
+
+    // This function is called one time after the preload scene, it is suitable for creating objects instances and generating the static environment
+    create(){
+        this.fpsText = this.add.bitmapText(8, 2, 'CursedScript', 'FPS: ', FONT_SIZE_TITLE).setTint(0xFFFFFF);
+    }
+
+    update(time){
+        this.deltaTime = time - this.lastFrameTime;
+        this.lastFrameTime = time;
+        
+        this.fpsText.setText(`FPS: ${Number.parseFloat(1/(this.deltaTime/1000)).toFixed()}`);
     }
 }
 // #endregion
@@ -262,12 +316,12 @@ function onGamepadDisconnect(){
 
 function onButton(){
     inputX = gamepadButtons.right.pressed - gamepadButtons.left.pressed;
-    inputJump = gamepadButtons.jump.pressed;
+    inputJump = + gamepadButtons.jump.pressed; // + converts bool to int
 }
 
 function onKey(){
     inputX = keyboardKeys.right.isDown - keyboardKeys.left.isDown;
-    inputJump = keyboardKeys.jump.isDown;
+    inputJump = + keyboardKeys.jump.isDown; // + converts bool to int
 }
 
 //function onKeyDown()
@@ -282,7 +336,7 @@ const config = {
         default: 'arcade',
         arcade: {
             gravity: { y: 981 }, // 9,81 m.s-2 sur Terre
-            debug: true
+            debug: DEBUG
         }
     },
     render: {
@@ -290,7 +344,8 @@ const config = {
     },
     scene: [
         GameScene,
-        GameHUDScene
+        GameHUDScene,
+        DebugScene
     ],
     input: {
         gamepad: true,
